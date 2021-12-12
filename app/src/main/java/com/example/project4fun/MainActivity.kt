36bbce568eb972253a49e70dce7333eb28722f19
jellybeansenjoyer <therealraghavkashyap26@gmail.com>
2021-12-12 21:00:42 +0530

    Project Creation, DataBinding dependencies added, ActivityMainBinding set

---
+++ app/src/main/java/com/example/project4fun/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.project4fun
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.project4fun.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit private var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+    }
+}
